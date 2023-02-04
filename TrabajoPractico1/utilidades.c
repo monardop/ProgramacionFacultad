d607b0a662bf275651c7bf6b3d1b8021428ff5ec
Pablo <pablomonardo@gmail.com>
2023-02-04 17:29:48 -0300

    Creé un archivo con utilidades del tp

---
+++ TrabajoPractico1/utilidades.c
@@ -0,0 +1,34 @@
+#include "utilidades.h"
+#include <stdio.h>
+
+
+double abs(double n)
+{
+    if(n < 0)
+    {
+        return -n;
+    }
+    else
+    {
+        return n;
+    }
+}
+
+double pow(double base, int exponente)
+{
+    double potencia = 1;
+    for (size_t i = 1; i <= exponente; i++)
+    {
+        potencia *= base;
+    }
+    return potencia;
+}
+
+double fact(double n)
+{
+    double fact = 1;
+    for(int i = 1; i <= n; i++){
+        fact *= i;
+    }
+    return fact;
+}
---
+++ TrabajoPractico1/utilidades.h
@@ -0,0 +1,10 @@
+#ifndef UTILIDADES_H
+#define UTILIDADES_H
+
+#define enum bool {"Verdadero", "Falso"};
+
+double abs(double n);
+double pow(double base, int exponente);
+double fact(double n);
+
+#endif
