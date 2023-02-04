35d2a72498d92137ae20e352e7939157c2c7f623
Pablo <pablomonardo@gmail.com>
2023-02-04 18:18:49 -0300

    Finalizado

--- TrabajoPractico1/ejercicio7.c
+++ TrabajoPractico1/ejercicio7.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
-#include "utilidades.h"
 
 
 int main()
 {
-    
+    int numeroDeseado, respuesta;
+
+    printf("Ingresar un numero: ");
+    scanf("%d", &numeroDeseado);
+
+    respuesta = 0;
+    for(int i = 1; i < numeroDeseado; i++)
+    {
+        if(numeroDeseado % i == 0)
+        {
+            respuesta += i;
+        }
+    } 
+
+    if(respuesta == numeroDeseado)
+    {
+        printf("Numero perfecto.\n");
+    }
+    else if (respuesta < numeroDeseado)
+    {
+        printf("Numero deficiente.\n");
+    }
+    else
+    {
+        printf("Numero abundante.\n");
+    }
+
     return 0;
 }
