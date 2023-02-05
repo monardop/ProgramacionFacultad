875efcb319b94862c6e06499b0e6936200b30978
Pablo <pablomonardo@gmail.com>
2023-02-05 10:39:35 -0300

    Ejercicio 10 finalizado
    feat: El usuario ingresa un numero x y el programa devuelve la suma de los x primeros pares

--- TrabajoPractico1/ejercicio10.c
+++ TrabajoPractico1/ejercicio10.c
@@ -2,5 +2,19 @@
 
 int main()
 {
+    int cantidadDeseada, suma = 0, contadorPares = 0;
+    int i = 0;
+    printf("Ingrese cuantos numeros pares desea: ");
+    scanf("%d", &cantidadDeseada);
+
+    while(contadorPares != cantidadDeseada)
+    {
+        suma += i;
+        i += 2;
+        contadorPares++;
+    }
+
+    printf("El resultado: %d", suma);
     
+    return 0;
 }
