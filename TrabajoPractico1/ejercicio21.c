46782958bbc2864529040a23147e23af4824deca
Pablo <pablomonardo@gmail.com>
2023-02-09 19:35:29 -0300

    Ejercicio finalizado

---
+++ TrabajoPractico1/ejercicio21.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int nro;
+    float real;
+    printf("Inserte un numero decimal: ");
+    scanf("%f", &real);
+    nro = real;
+    printf("%d", nro);
+
+    return 0;
+}
