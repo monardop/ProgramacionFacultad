22d8383c6674e9736b5b475bc623ab2152a0b018
Pablo <pablomonardo@gmail.com>
2023-02-17 15:52:29 -0300

    Modulo
    Creo un modulo que se encarga de operaciones individuales entre matrices

---
+++ TrabajoPractico1/Matrices/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "moduloMatrices.h"
+
+int main()
+{
+    int matrizA[4][4], matrizB[4][4];
+}
---
+++ TrabajoPractico1/Matrices/moduloMatrices.h
@@ -0,0 +1,12 @@
+#ifndef MODULOMATRICES_H
+
+#define MODULOMATRICES_H
+
+void sumatoriaTriangularSuperior();
+void sumatoriaTriangularInferior();
+void trazaPrincipalSecundaria();
+void esSimetrica();
+void trasponerMatriz();
+
+#include "moduloMatrices.c"
+#endif
