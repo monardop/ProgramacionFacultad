399e70d693464cefafe12cdb0cfb4fa7f84a598d
Pablo <pablomonardo@gmail.com>
2023-02-20 21:41:12 -0300

    Nueva funcion

--- TrabajoPractico1/Matrices/moduloMatrices.c
+++ TrabajoPractico1/Matrices/moduloMatrices.c
@@ -15,3 +15,20 @@ void sumatoriaTriangularSuperior(const int *p, int fila, int columna)
     }
     printf("El resultado es: %d", sumatoria);
 }
+
+void sumatoriaTriangularInferior(const int*, int, int)
+{
+    int sumatoria = 0;
+    for(size_t i = 0; i < fila; i++)
+    {
+        for (size_t j = 0; j < columna; j++)
+        {
+            if (i > j)
+            {
+                sumatoria += *(p + i * columna + j);
+            }
+        }
+    }
+    printf("El resultado es: %d", sumatoria);
+}
+
