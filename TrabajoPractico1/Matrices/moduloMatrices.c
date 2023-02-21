68bac4a52133676109a350b0eca6b0eaf34b80a6
Pablo <pablomonardo@gmail.com>
2023-02-20 21:52:18 -0300

    Update moduloMatrices.c

--- TrabajoPractico1/Matrices/moduloMatrices.c
+++ TrabajoPractico1/Matrices/moduloMatrices.c
@@ -1,34 +1,54 @@
 #include "moduloMatrices.h"
 
-void sumatoriaTriangularSuperior(const int *p, int fila, int columna)
+void sumatoriaTriangularSuperior(const int *p, int dimension)
 {
     int sumatoria = 0;
-    for(size_t i = 0; i < fila; i++)
+    for(size_t i = 0; i < dimension; i++)
     {
-        for (size_t j = 0; j < columna; j++)
+        for (size_t j = 0; j < dimension; j++)
         {
             if (i < j)
             {
-                sumatoria += *(p + i * columna + j);
+                sumatoria += *(p + i * dimension + j);
             }
         }
     }
     printf("El resultado es: %d", sumatoria);
 }
 
-void sumatoriaTriangularInferior(const int*, int, int)
+void sumatoriaTriangularInferior(const int *p, int dimension)
 {
     int sumatoria = 0;
-    for(size_t i = 0; i < fila; i++)
+    for(size_t i = 0; i < dimension; i++)
     {
-        for (size_t j = 0; j < columna; j++)
+        for (size_t j = 0; j < dimension; j++)
         {
             if (i > j)
             {
-                sumatoria += *(p + i * columna + j);
+                sumatoria += *(p + i * dimension + j);
             }
         }
     }
     printf("El resultado es: %d", sumatoria);
 }
 
+void trazaPrincipalSecundaria(const int *p, int dimension)
+{
+    int diagonalPrincipal = 0, diagonalSecundaria = 0;
+    for(size_t i = 0; i < dimension; i++)
+    {
+        for (size_t j = 0; j < dimension; j++)
+        {
+            if (i > j)
+            {
+                diagonalPrincipal += *(p + i * dimension + j);
+            }
+        }
+    }
+    printf("Diagonal principal: %d\n", diagonalPrincipal);
+    for (size_t i = 0; i < dimension; i++)
+    {
+        diagonalSecundaria += *(p + i * dimension + ((dimension - 1 )-i));
+    }
+    printf("Diagonal secundaria: %d", diagonalSecundaria);
+}
